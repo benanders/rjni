96456305375e9d08697eb49daa84098b7ea0f781
Ben Anderson <gravityscore@gmail.com>
2014-12-21 00:21:50 +0800

    Updated dependencies, fixed compilation issues

--- build.rs
+++ build.rs
@@ -6,6 +6,7 @@ fn main() {
 		include_directories: vec![Path::new("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers")],
 		definitions: Vec::new(),
 		objects: Vec::new(),
+		flags: Vec::new(),
 	};
 
 	gcc::compile_library("libinterface.a", &config, &["src/interface.c"]);
